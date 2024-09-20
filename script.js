1a999f134db7483697e769d209e10bce27b022ea
Tybouddha <bruneecedric@gmail.com>
2024-09-20 16:27:49 +0200

    frontend

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 fetch('https://whatyear-backend-tawny.vercel.app/year')
     .then(response => response.json())
     .then(data => {
-    console.log(data.year);
     document.querySelector('#year').textContent = data.year
 });
 
